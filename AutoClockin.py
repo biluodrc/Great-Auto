5bba7476465ccd0b0ee4e76702a5074cfe315506
BiLuo <44452689+biluodrc@users.noreply.github.com>
2020-08-18 11:51:57 +0800

    更新添加信息部分

--- AutoClockin.py
+++ AutoClockin.py
@@ -5,11 +5,10 @@
 
 from selenium import webdriver
 from time import sleep
+import datetime
 
-# username: 打卡网站的用户名
-# password: 密码
-username = 'username' 
-password = 'password'
+username = 'dengrc5518'
+password = 'drc991102'
 
 broswer = webdriver.Chrome()
 broswer.get('https://ehall.jlu.edu.cn/infoplus/form/BKSMRDK/start')
@@ -24,6 +23,41 @@ loginB.click()
 sleep(3)
 
 # clock in
+# 选择校区
+options = broswer.find_elements_by_tag_name('option')
+for i in range(len(options)):
+    if (options[i].text == '-请选择-'):
+        options[i].click()
+    if (options[i].text == '中心校区'):
+        options[i].click()
+        break
+# 选择寝室位置
+options = broswer.find_elements_by_tag_name('option')
+for i in range(len(options)):
+    if (options[i].text == '-请选择-'):
+        options[i].click()
+    if (options[i].text == '北苑1公寓'):
+        options[i].click()
+        break
+# 输入寝室号
+qsI = broswer.find_element_by_name('fieldSQqsh')
+qsI.send_keys('2076')
+
+hour = int(datetime.datetime.now().strftime('%H'))
+labels = broswer.find_elements_by_tag_name('label')
+if (hour == 7):
+    for label in labels:
+        if (label.text == '正常'):
+            label.click()
+if (hour == 11):
+    for label in labels:
+        if (label.text == '正常'):
+            label.click()
+if (hour == 17):
+    for label in labels:
+        if (label.text == '正常'):
+            label.click()
+# 提交
 submitB = broswer.find_elements_by_class_name('command_button_content')
 submitB[0].click()
 sleep(3)
